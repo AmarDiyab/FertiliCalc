5e7321ca86f0143e8d85e080cef0645ee18bc23e
Amar Diyab <diyabamar3043@gmail.com>
2023-11-10 01:01:14 +0530

    Added Home page

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../assets/logo.svg";
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center bg-[#ebf9f5]'>
+        <Image src={logo} alt="LOGO" width={80} className='py-5 px-5' />
+        <span className='text-[#f68c90] font-mono font-bold '>Fertili Calc</span>
+    </div>
+  )
+}
+
+export default Navbar
