5bc3c93d5ba4944d50984f8e21141c47e2d0a73b
Amar Diyab <diyabamar3043@gmail.com>
2023-11-10 01:10:50 +0530

    Updated styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className='flex items-center bg-[#ebf9f5]'>
+    <div className='flex items-center bg-[#b3e8da]'>
         <Image src={logo} alt="LOGO" width={80} className='py-5 px-5' />
-        <span className='text-[#f68c90] font-mono font-bold '>Fertili Calc</span>
+        <span className='text-[#f68c90] text-xl font-mono font-bold '>Fertili Calc</span>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -4,40 +4,40 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="mx-[5rem] mt-10">
-      <span className="text-2xl font-semibold">CALCULATORS</span>
+      <div className="mx-[10rem] mt-10">
+      <span className="text-2xl font-semibold text-[#f68c90]">CALCULATORS</span>
       <div className="grid grid-cols-4 gap-10 mt-10">
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:ease-in hover:bg-[#c4e9de] ">
           <span className="font-semibold text-lg">PREGNANCY</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] ">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de] ">
           <span className="font-semibold text-lg">PREGNANCY WEIGHT GAIN</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de]">
           <span className="font-semibold text-lg">PREGNANCY CONCEPTION</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de]">
           <span className="font-semibold text-lg">DUE DATE</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de]">
           <span className="font-semibold text-lg">OVULATION</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de]">
           <span className="font-semibold text-lg">CONCEPTION</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
 
-        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px]">
+        <div className="flex flex-col px-7 py-7 space-y-5 border rounded-lg h-[240px] w-[240px] bg-[#a5dccd] hover:bg-[#c4e9de]">
           <span className="font-semibold text-lg">PERIOD</span>
           <span className="font-light text-sm">estimates due date based on last period, ultrasound, conception, or IVF transfer date.</span>
         </div>
