28f4502f64fb784301cff5dc89c54d3155928b79
Amar Diyab <diyabamar3043@gmail.com>
2023-11-11 10:34:13 +0530

    Added Start Date Result in ConceptionDateForm

--- app/pregnancy/ConceptionDateForm.jsx
+++ app/pregnancy/ConceptionDateForm.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { setStartDate } from "../redux/pregnancySlice";
 
 const ConceptionDateForm = () => {
+    const dispatch = useDispatch();
+
+    const form = useForm();
+    const { register, handleSubmit }  = form
+
+    const calculateStartDate = (conceptionDate) => {
+        const adjustedDate = new Date(conceptionDate);
+    adjustedDate.setDate(adjustedDate.getDate() + 14);
+    adjustedDate.setDate(adjustedDate.getDate() - 266);
+
+    // Format the calculated date
+    const startDate = adjustedDate.toISOString().split("T")[0];
+
+    return startDate
+    }
+
+    const onSubmit = (data) => {
+        const conceptionDate = data["date"]
+
+        const startDate = calculateStartDate(conceptionDate)
+
+        console.log(startDate);
+        dispatch(setStartDate(startDate));
+    }
+
+
   return (
     <div>
+        <form onSubmit={handleSubmit(onSubmit)} >
       <div className="flex flex-col space-y-10">
         <div className="flex justify-between items-center">
           <span className="font-semibold">Conception Date</span>
-          <input type="date" className="px-2 py-2 rounded-md w-44" />
+          <input type="date" id="date" {...register("date")} className="px-2 py-2 rounded-md w-44" />
         </div>
         <div className="flex justify-center">
           <button
@@ -17,6 +47,7 @@ const ConceptionDateForm = () => {
           </button>
         </div>
       </div>
+      </form>
     </div>
   );
 };
